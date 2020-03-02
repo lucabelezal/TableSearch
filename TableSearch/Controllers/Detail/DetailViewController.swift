522f1b6630283e9788452809d997acaf1c8107cc
lucasnascimento <lucabelezal@gmail.com>
2020-03-02 16:20:28 -0300

    Add default project

--- TableSearch/Controllers/Detail/DetailViewController.swift
+++ TableSearch/Controllers/Detail/DetailViewController.swift
@@ -9,11 +9,70 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    // Constants for state restoration.
+    private static let restoreProduct = "restoreProductKey"
+    
+    // MARK: - Properties
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var product: Product!
+    
+    @IBOutlet private weak var yearLabel: UILabel!
+    @IBOutlet private weak var priceLabel: UILabel!
+    
+    // MARK: - Initialization
+    
+    class func detailViewControllerForProduct(_ product: Product) -> UIViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+
+        let viewController =
+            storyboard.instantiateViewController(withIdentifier: "DetailViewController")
+        
+        if let detailViewController = viewController as? DetailViewController {
+            detailViewController.product = product
+        }
+        
+        return viewController
     }
+    
+    // MARK: - View Life Cycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
+        title = product.title
+        
+        yearLabel.text = "\(product.yearIntroduced)"
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.formatterBehavior = .default
+        let priceString = numberFormatter.string(from: NSNumber(value: product.introPrice))
+        priceLabel.text = priceString
+    }
+    
+}
 
+// MARK: - UIStateRestoration
+
+extension DetailViewController {
+    
+    override func encodeRestorableState(with coder: NSCoder) {
+        super.encodeRestorableState(with: coder)
+        
+        // Encode the product.
+        coder.encode(product, forKey: DetailViewController.restoreProduct)
+    }
+    
+    override func decodeRestorableState(with coder: NSCoder) {
+        super.decodeRestorableState(with: coder)
+        
+        // Restore the product.
+        if let decodedProduct = coder.decodeObject(forKey: DetailViewController.restoreProduct) as? Product {
+            product = decodedProduct
+        } else {
+            fatalError("A product did not exist. In your app, handle this gracefully.")
+        }
+    }
+    
 }
--- TableSearch/Controllers/Results/ResultsTableController.swift
+++ TableSearch/Controllers/Results/ResultsTableController.swift
@@ -9,11 +9,35 @@
 import UIKit
 
 class ResultsTableController: UITableViewController {
-
+    
+    let tableViewCellIdentifier = "cellID"
+    
+    var filteredProducts = [Product]()
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let nib = UINib(nibName: "TableCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: tableViewCellIdentifier)
     }
-
-
+    
+    // MARK: - UITableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredProducts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: tableViewCellIdentifier, for: indexPath)
+        let product = filteredProducts[indexPath.row]
+        
+        cell.textLabel?.text = product.title
+        
+        let priceString = product.formattedIntroPrice()
+        cell.detailTextLabel?.text = "\(priceString!) | \(product.yearIntroduced)"
+        
+        return cell
+    }
 }
