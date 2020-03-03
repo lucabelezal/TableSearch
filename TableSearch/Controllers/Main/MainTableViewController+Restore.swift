e83976de0803b8791290566c9d1e384ef7518587
lucasnascimento <lucabelezal@gmail.com>
2020-03-03 20:41:18 -0300

    Fix marks

--- TableSearch/Controllers/Main/MainTableViewController+Restore.swift
+++ TableSearch/Controllers/Main/MainTableViewController+Restore.swift
@@ -10,8 +10,9 @@ import UIKit
 
 extension MainTableViewController {
     
-    /// State restoration values.
-    enum RestorationKeys: String {
+    // MARK: - State restoration values
+    
+    enum RestorationKeys: String, CodingKey {
         case viewControllerTitle
         case searchControllerIsActive
         case searchBarText
@@ -19,52 +20,37 @@ extension MainTableViewController {
         case selectedScope
     }
     
-    // State items to be restored in viewDidAppear().
+    // MARK: - State items to be restored in viewDidAppear()
+    
     struct SearchControllerRestorableState {
         var wasActive = false
         var wasFirstResponder = false
     }
     
+    // MARK: - Encode the view state so it can be restored later
+    
     override func encodeRestorableState(with coder: NSCoder) {
         super.encodeRestorableState(with: coder)
-        
-        // Encode the view state so it can be restored later.
-        
-        // Encode the title.
         coder.encode(navigationItem.title!, forKey: RestorationKeys.viewControllerTitle.rawValue)
-
-        // Encode the search controller's active state.
         coder.encode(searchController.isActive, forKey: RestorationKeys.searchControllerIsActive.rawValue)
-        
-        // Encode the first responser status.
         coder.encode(searchController.searchBar.isFirstResponder, forKey: RestorationKeys.searchBarIsFirstResponder.rawValue)
-        
-        // Encode the first responser status.
         coder.encode(searchController.searchBar.selectedScopeButtonIndex, forKey: RestorationKeys.selectedScope.rawValue)
-        
-        // Encode the search bar text.
         coder.encode(searchController.searchBar.text, forKey: RestorationKeys.searchBarText.rawValue)
     }
+    
+    // MARK: - Restore the view state
     
     override func decodeRestorableState(with coder: NSCoder) {
         super.decodeRestorableState(with: coder)
         
-        // Restore the title.
         guard let decodedTitle = coder.decodeObject(forKey: RestorationKeys.viewControllerTitle.rawValue) as? String else {
             fatalError("A title did not exist. In your app, handle this gracefully.")
         }
-        navigationItem.title! = decodedTitle
         
-        /** Restore the active and first responder state:
-            We can't make the searchController active here since it's not part of the view hierarchy yet, instead we do it in viewDidAppear.
-        */
+        navigationItem.title = decodedTitle
         restoredState.wasActive = coder.decodeBool(forKey: RestorationKeys.searchControllerIsActive.rawValue)
         restoredState.wasFirstResponder = coder.decodeBool(forKey: RestorationKeys.searchBarIsFirstResponder.rawValue)
-        
-        // Restore the scope bar selection.
         searchController.searchBar.selectedScopeButtonIndex = coder.decodeInteger(forKey: RestorationKeys.selectedScope.rawValue)
-        
-        // Restore the text in the search field.
         searchController.searchBar.text = coder.decodeObject(forKey: RestorationKeys.searchBarText.rawValue) as? String
     }
     
